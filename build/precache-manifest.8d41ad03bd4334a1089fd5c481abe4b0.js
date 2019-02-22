3ce0237452d42f18431b523d962aa0b396d22d61
Maryana <sentsiv.m@gmail.com>
2019-02-22 14:12:36 +0200

    deploy1

---
+++ build/precache-manifest.8d41ad03bd4334a1089fd5c481abe4b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tic-tac-toe/build/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7c74a0a18059363ec640",
+    "url": "/tic-tac-toe/build/static/js/runtime~main.7c74a0a1.js"
+  },
+  {
+    "revision": "f0453b74ed27c737e9dc",
+    "url": "/tic-tac-toe/build/static/js/main.f0453b74.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/tic-tac-toe/build/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "f0453b74ed27c737e9dc",
+    "url": "/tic-tac-toe/build/static/css/main.1b38c7e4.chunk.css"
+  },
+  {
+    "revision": "c1fed4fcb850ca5a1df692e5fe8696c5",
+    "url": "/tic-tac-toe/build/index.html"
+  }
+];
